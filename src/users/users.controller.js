fdf3b5d8176b85d24ff82a5e0c72ddf20a0c5dc1
Stanislav Pastushok <stan3slaw@gmail.com>
2022-10-21 17:53:06 +0300

    fixed exportToXml method, users controller and service structure

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -3,39 +3,41 @@ const xml2js = require('xml2js');
 const usersService = require('./users.service');
 const mockedUsers = require('../core/mock-data');
 
-const find = (req, res) => {
-  if (req.params.id) {
-    return res.send(mockedUsers.find((user) => user.id === Number(req.params.id)));
-  }
+class UsersController {
+  find = (req, res) => {
+    if (req.params.id) {
+      return res.send(mockedUsers.find((user) => user.id === Number(req.params.id)));
+    }
 
-  return res.send(mockedUsers);
-};
+    return res.send(mockedUsers);
+  };
 
-const create = (req, res) => {
-  const user = req.body;
-  mockedUsers.push(user);
-  res.send(user);
-};
+  create = (req, res) => {
+    const user = req.body;
+    mockedUsers.push(user);
+    res.send(user);
+  };
 
-const createFromFile = (req, res) => {
-  const { users } = req.body;
+  createFromFile = (req, res) => {
+    const { users } = req.body;
 
-  const normalizedResponse = usersService.normalizeResponse(users.user, mockedUsers);
+    const normalizedResponse = usersService.normalizeResponse(users.user, mockedUsers);
 
-  res.send(normalizedResponse);
-};
+    res.send(normalizedResponse);
+  };
 
-const exportToXml = (req, res) => {
-  const filePath = 'users.xml';
-  const builder = new xml2js.Builder();
-  const usersInXml = builder.buildObject({ users: { user: mockedUsers } });
+  exportToXml = async (req, res) => {
+    const filePath = 'users.xml';
+    const builder = new xml2js.Builder();
+    const usersInXml = builder.buildObject({ users: { user: mockedUsers } });
 
-  usersService.exportUsersToXml(req, res, usersInXml, filePath);
-};
+    res.writeHead(200, {
+      'Content-Type': 'application/octet-stream',
+      'Content-Disposition': `attachment; filename=${filePath}`,
+    });
+    res.write(usersInXml);
+    res.end();
+  };
+}
 
-module.exports = {
-  find,
-  create,
-  createFromFile,
-  exportToXml,
-};
+module.exports = new UsersController();
--- src/users/users.service.js
+++ src/users/users.service.js
@@ -1,66 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+class UsersService {
+  normalizeResponse = (users, mockedUsers) => {
+    let normalizedUsers;
+    if (Array.isArray(users)) {
+      normalizedUsers = users.map((user) => ({ ...user, id: Number(user.id) }));
+      mockedUsers.push(...normalizedUsers);
+    } else {
+      normalizedUsers = { ...users, id: Number(users.id) };
+      mockedUsers.push(normalizedUsers);
+    }
 
-const normalizeResponse = (users, mockedUsers) => {
-  let normalizedUsers;
-  if (Array.isArray(users)) {
-    normalizedUsers = users.map((user) => ({ ...user, id: Number(user.id) }));
-    mockedUsers.push(...normalizedUsers);
-  } else {
-    normalizedUsers = { ...users, id: Number(users.id) };
-    mockedUsers.push(normalizedUsers);
-  }
+    return normalizedUsers;
+  };
+}
 
-  return normalizedUsers;
-};
-
-const exportUsersToXml = (_, res, usersInXml, filePath) => {
-  const ws = fs.createWriteStream(filePath, 'utf-8');
-  ws.write(usersInXml);
-
-  ws.on('error', (err) => {
-    console.error('write stream error', err);
-  });
-
-  ws.close(() => {
-    fs.access(filePath, (fileAccessErr) => {
-      if (fileAccessErr) {
-        console.error('file access error', fileAccessErr);
-      }
-
-      const rs = fs.createReadStream(filePath, 'utf-8');
-      let data = '';
-
-      rs.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      rs.on('error', (err) => {
-        console.error('read stream error', err);
-      });
-
-      rs.on('end', () => {
-        res.writeHead(200, {
-          'Content-Type': 'application/octet-stream',
-          'Content-Disposition': `attachment; filename=${filePath}`,
-        });
-
-        res.write(data);
-        res.end();
-      });
-
-      rs.on('close', () => {
-        fs.unlink(path.resolve(filePath), (err) => {
-          if (err) {
-            console.error('deleting file error', err);
-          }
-        });
-      });
-    });
-  });
-};
-
-module.exports = {
-  normalizeResponse,
-  exportUsersToXml,
-};
+module.exports = new UsersService();
